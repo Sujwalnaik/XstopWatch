2983c10366a720dd328fa059ede8878f127a0b98
Sujwalnaik <sujwalbn.a0@gmail.com>
2024-07-03 22:48:48 +0530

    created Stop Watch

---
+++ XStopWatch/src/component/Stopwatch.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else if (!isRunning && time !== 0) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning, time]);
+
+  const formatTime = (time) => {
+    const getSeconds = `0${time % 60}`.slice(-2);
+    const minutes = `${Math.floor(time / 60)}`;
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+
+    return `${getMinutes}:${getSeconds}`;
+  };
+
+  const handleStartStop = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  return (
+    <div>
+      <h1>StopWatch</h1>
+      <div style={{ fontSize: "30px" }}>{formatTime(time)}</div>
+      <div style={{ display: "flex", gap: 20, justifyContent: "center" }}>
+        <button onClick={handleStartStop}>
+          {isRunning ? "Stop" : "Start"}
+        </button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default Stopwatch;
